5d53822bef801e7709186e6ab30ca1fbde199b40
arpit8929 <arpit.devganiya02@gmail.com>
2025-10-10 12:24:18 +0530

    output & version done

--- website/prediction.py
+++ website/prediction.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, send_from_directory
-from .app_functions import ValuePredictor, pred
+from .app_functions import ValuePredictor
 import os
 from werkzeug.utils import secure_filename
 
@@ -24,25 +24,7 @@ def predict():
     else:
         return render_template( 'base.html')
 
-@prediction.route('/upload', methods=['POST','GET'])
-def upload_file():
-    if request.method=="GET":
-        return render_template('pneumonia.html', title='Pneumonia Disease')
-    else:
-        file = request.files["file"]
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath,'uploads',  secure_filename(file.filename))
-        file.save(file_path)
-        indices = {0: 'Normal', 1: 'Pneumonia'}
-        result = pred(file_path)
-
-        if result>0.5:
-            label = indices[1]
-            accuracy = result * 100
-        else:
-            label = indices[0]
-            accuracy = 100 - result
-        return render_template('deep_pred.html', image_file_name=file.filename, label = label, accuracy = accuracy)
+## Removed pneumonia upload route as only heart model is used now
 
 @prediction.route('/uploads/<filename>')
 def send_file(filename):
