2a133bf7c095d2e457ae4a3833dc43480a99f17a
arpit8929 <arpit.devganiya02@gmail.com>
2025-10-11 00:39:58 +0530

    recommendation feature

--- website/views.py
+++ website/views.py
@@ -9,6 +9,13 @@ def home():
 def heart():
     return render_template(r'heart_index.html')
 
+@views.route('/recommend/<int:prediction_status>')
+def show_recommendation(prediction_status):
+   
+    # The render_template function automatically looks inside the 'templates' folder.
+    # We pass the 'prediction_status' from the URL to a variable named 'prediction' inside the HTML.
+    return render_template('recommendations.html', prediction=prediction_status)
+
 @views.route("/heart_form")
 def heart_form():
     return render_template(r'heart.html')
